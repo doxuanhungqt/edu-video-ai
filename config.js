eb0a15861694c89eb3e41ae45a312f0d8b0b601f
doxuanhungqt <doxuanhungqt@gmail.com>
2026-09-21 11:52:07 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.EDU_CONFIG = {
+  SUPABASE_URL: "https://supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_...",
+  OWNER_EMAIL: "xuanhungxb2013@gmail.com",
+  ACCESS_FUNCTION_URL: ""
+};

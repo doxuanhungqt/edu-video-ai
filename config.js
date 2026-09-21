e9ef3d20e34fe3ebb6345ec285e169cd36a6c11e
doxuanhungqt <doxuanhungqt@gmail.com>
2026-09-21 18:43:59 +0700

    Update Supabase config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.EDU_CONFIG = {
-  SUPABASE_URL: "https://supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_...",
+  SUPABASE_URL: "https://vjaqztuhkmayosrprix.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_uwHW3WKyBHTul5-wDI1Fzw_FibepIjg",
   OWNER_EMAIL: "xuanhungxb2013@gmail.com",
   ACCESS_FUNCTION_URL: ""
 };
